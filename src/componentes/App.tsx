64bd32b53ce992f966ce5296b86058d6efa7f502
igorrm <igorrodriguesmachado23@gmail.com>
2024-05-13 04:43:58 -0300

    1, Iniciando projeto jogo no react

---
+++ src/componentes/App.tsx
@@ -0,0 +1,29 @@
+//import { useState } from 'react'
+import './App.css'
+import Board from './board/index'
+
+
+
+function App() {
+
+
+  return (
+   <div className='classApp'>
+     <main className='classApp-main'>
+       <Board/>
+     </main>
+   </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
