ad0a0876ba1f19fc5264a89e76fd70d3847a098d
igorrm19 <igorrodiguesmachado23@gmail.com>
2024-12-20 22:11:33 -0300

    Mover

--- src/componentes/hero/index.tsx
+++ src/componentes/hero/index.tsx
@@ -3,31 +3,80 @@ import React from 'react'
 import './index.css'
 import { HERO_HEMET_OFFSET, TILE_SIZE } from '../../settings/constantes';
 
-function hadclick(event){
-  console.log("Deu certo", event.key)
-}
+
+// function hadclick(event){
+
+//   if(event.key === "l"){
+//     setTimeout(() => {
+//       const newPosition = {x: 30, y:0};
+//       positionStateUpdate(newPosition);
+  
+//     }, 2000)
+//   }
+
+//   console.log("Deu certo", event.key);
+
+// }
 
 const initialPosition = {
-  x: 1, 
-  y: 1
+  x: 10, 
+  y: 10
 }
 
 const Hero = () => {
 
   const [positionState, positionStateUpdate] = React.useState(initialPosition);
+
+
+   const Mover = (event) => {
+    
+    if(event.key === "ArrowLeft"){
+
+     const newState = {
+      x: positionState.x,
+      y: positionState.y - 3, 
+     }
+
+     positionStateUpdate(newState);
+
+     }else if(event.key === "ArrowRight"){
+
+      const newState = {
+        x: positionState.x ,
+        y: positionState.y + 3, 
+       }
+  
+       positionStateUpdate(newState);
   
 
-  useEventListener('keydown', hadclick);
+     }else if(event.key === "ArrowDown"){
 
-
-  setTimeout(() => {
-    const newPosition = {x: 30, y:0};
-    positionStateUpdate(newPosition);
-
-  }, 2000)
-
+      const newState = {
+        x: positionState.x - 3,
+        y: positionState.y, 
+       }
+  
+       positionStateUpdate(newState);
   
 
+     }else if(event.key === "ArrowUp"){
+
+      const newState = {
+        x: positionState.x + 3,
+        y: positionState.y, 
+       }
+  
+       positionStateUpdate(newState);
+  
+
+     }
+
+
+   }
+
+
+   useEventListener('keydown', Mover);
+  
    return(
     <>
       <div 
